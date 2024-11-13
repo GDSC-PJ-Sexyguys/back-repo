fb16c1d1abe2d4433bbc5bbb50e1ded84a96c52a
kwonyonghyun <rnjs990523@naver.com>
2024-11-13 16:02:21 +0900

    initial commit

---
+++ src/test/java/com/example/GDSC_hackathon/GdscHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GDSC_hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdscHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
